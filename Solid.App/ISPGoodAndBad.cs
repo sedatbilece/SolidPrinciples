1fd64c2da6fdf52af24c46e5f246fd379c0d0cb7
sedatbilece <seda1tbilece@gmail.com>
2023-10-08 14:52:42 +0300

    add Interface Segregation

---
+++ Solid.App/ISPGoodAndBad.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solid.App.ISPGoodAndBad
+{
+      public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+
+    public interface IReadRepository
+    {
+        List<Product> GetAll();
+
+        Product GetById(int id);
+    }
+
+
+    public interface IWriteRepository
+    {
+        Product Create(Product p);
+        Product Update(Product p);
+        Product Delete(Product p);
+    }
+
+}
