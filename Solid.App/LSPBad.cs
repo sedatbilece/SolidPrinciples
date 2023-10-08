3caacf136306321b3754a27dd5c6f32085d414ac
sedatbilece <seda1tbilece@gmail.com>
2023-10-08 14:23:43 +0300

    add Liskov Substitution

---
+++ Solid.App/LSPBad.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solid.App.LSPBad
+{
+    public interface ITakePhoto
+    {
+        void TakePhoto();
+    }
+
+    public  class BasePhone
+    {
+        public void Call()
+        {
+            Console.WriteLine("Arama yapıldı.");
+        }
+
+    }
+
+    public class Iphone : BasePhone, ITakePhoto
+    {
+        public void TakePhoto()
+        {
+            Console.WriteLine("foto çekildi");
+        }
+    }
+
+    public class Nokia : BasePhone
+    {
+
+    }
+}
---
+++ Solid.App/LSPGood.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solid.App.LSPGood
+{
+    public abstract class BasePhone
+    {
+        public void Call()
+        {
+            Console.WriteLine("Arama yapıldı.");
+        }
+
+        public abstract void TakePhoto(); 
+
+    }
+
+
+    public class Iphone : BasePhone
+    {
+        public override void TakePhoto()
+        {
+            Console.WriteLine("Fotoğraf çekildi.");
+        }
+    }
+    // nokia foto çekmeyi desteklemiyor ise TakePhoto() adlı fonksiyona erişilememeli
+
+    public class Nokia : BasePhone
+    {
+        public override void TakePhoto()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
